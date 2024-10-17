15a8c79eaafbada8a1a53f25f915d493ad6586be
Paraldos <hillbrecht.s@gmail.com>
2024-10-17 16:21:48 +0200

    refactored tableus

--- components/gameBoard.js
+++ components/gameBoard.js
@@ -3,70 +3,13 @@ import PlayingCard from "./playingCard.js";
 
 export default class GameBoard {
   constructor() {
-    this.container = this.addContainer();
-    this.stock = this.addStock();
-    this.waste = this.addWaste();
-    this.container.appendChild(document.createElement("div"));
-    this.foundation = [
-      this.addFoundation(),
-      this.addFoundation(),
-      this.addFoundation(),
-      this.addFoundation(),
-    ];
-    this.tableau = [
-      this.addTableau(),
-      this.addTableau(),
-      this.addTableau(),
-      this.addTableau(),
-      this.addTableau(),
-      this.addTableau(),
-      this.addTableau(),
-    ];
+    this.gameBoard = document.querySelector(".game-board");
+    this.stock = document.querySelector(".game-board__stock");
+    this.waste = document.querySelector(".game-board__waste");
+    this.foundation = document.querySelector(".game-board__foundation");
+    this.tableau = document.querySelectorAll(".game-board__tableau");
     this.playingCards = new PlayingCards().getCards();
     this.fillTableus();
-  }
-
-  addContainer() {
-    const container = document.createElement("div");
-    container.classList.add("game-board");
-    document.body.appendChild(container);
-    return container;
-  }
-
-  addStock() {
-    const stock = document.createElement("div");
-    stock.classList.add("game-board__stock");
-    stock.classList.add("game-board__card-field");
-    stock.appendChild(this.getPlaceholder());
-    this.container.appendChild(stock);
-    return stock;
-  }
-
-  addWaste() {
-    const waste = document.createElement("div");
-    waste.classList.add("game-board__waste");
-    waste.classList.add("game-board__card-field");
-    waste.appendChild(this.getPlaceholder());
-    this.container.appendChild(waste);
-    return waste;
-  }
-
-  addFoundation() {
-    const foundation = document.createElement("div");
-    foundation.classList.add("game-board__foundation");
-    foundation.classList.add("game-board__card-field");
-    foundation.appendChild(this.getPlaceholder());
-    this.container.appendChild(foundation);
-    return foundation;
-  }
-
-  addTableau() {
-    const tableau = document.createElement("div");
-    tableau.classList.add("game-board__tableau");
-    tableau.classList.add("game-board__card-field");
-    tableau.appendChild(this.getPlaceholder());
-    this.container.appendChild(tableau);
-    return tableau;
   }
 
   fillTableus() {
