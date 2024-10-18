cb283a9a968e50bca2f6a5c6f1e3f8476975506b
Paraldos <hillbrecht.s@gmail.com>
2024-10-19 01:02:52 +0200

    adjusted checkRankDistance

--- components/Card.js
+++ components/Card.js
@@ -3,8 +3,8 @@ import preload from "./preload.js";
 
 export default class Card {
   constructor(name) {
-    this.suit = name[0];
-    this.rank = name[1];
+    this.rank = name[0];
+    this.suit = name[1];
     this.clikable = FileSystemWritableFileStream;
     this.card = this.createCard(name);
     this.startX = 0;
@@ -20,6 +20,8 @@ export default class Card {
   createCard(name) {
     const card = document.createElement("div");
     card.classList.add("card");
+    card.dataset.rank = this.rank;
+    card.dataset.suit = this.suit;
     const cardInner = document.createElement("div");
     cardInner.classList.add("card__inner");
     cardInner.appendChild(preload.getSVG(`${"2B"}`));
@@ -33,6 +35,9 @@ export default class Card {
     if (this.isStock()) {
       this.onClickOnStock();
     }
+    if (this.isTableau()) {
+      this.onClickOnTableau();
+    }
   }
 
   onClickOnStock() {
@@ -40,6 +45,14 @@ export default class Card {
       document.body.dispatchEvent(new Event("resetStock"));
     } else {
       this.waste.appendChild(this.card);
+      this.flippCard(true);
+    }
+  }
+
+  onClickOnTableau() {
+    if (this.placeholder) {
+      return;
+    } else if (this.isLastChild()) {
       this.flippCard(true);
     }
   }
@@ -62,7 +75,14 @@ export default class Card {
     this.card.style.zIndex = 1000;
     this.card.style.setProperty("--transformX", `${newX}px`);
     this.card.style.setProperty("--transformY", `${newY}px`);
-    this.checkForFoundationOverlap();
+    if (this.isTableau() || this.isWaste()) {
+      const foundations = document.querySelectorAll(".game-board__foundation");
+      const overlap = this.getOverlap(foundations);
+      if (!overlap) return;
+      if (overlap.classList.contains("game-board__foundation")) {
+        this.checkOverlappingFoundation(overlap);
+      }
+    }
   }
 
   onMouseUp() {
@@ -71,12 +91,54 @@ export default class Card {
     this.card.style.zIndex = 0;
     document.removeEventListener("mousemove", this.onMouseMoveBound);
     document.removeEventListener("mouseup", this.onMouseUpBound);
+    this.onMouseUpOnPositiveOverlap();
     document.body.dispatchEvent(new Event("resetOverlapIndication"));
   }
 
-  // heper
+  // helper
+  onMouseUpOnPositiveOverlap() {
+    const positiveOverlap = document.querySelector(
+      ".game-board__positive-overlap"
+    );
+    if (positiveOverlap) {
+      positiveOverlap.appendChild(this.card);
+    }
+  }
+
+  checkOverlappingFoundation(overlap) {
+    const topCard = overlap.childNodes[overlap.childNodes.length - 1];
+
+    if (topCard.classList.contains("card__placeholder") && this.rank === "A") {
+      overlap.classList.add("game-board__positive-overlap");
+    } else if (
+      this.suit === topCard.dataset.suit &&
+      this.checkRankDistance(topCard.dataset.rank, this.rank) === 1
+    ) {
+      overlap.classList.add("game-board__positive-overlap");
+    } else {
+      overlap.classList.add("game-board__negative-overlap");
+    }
+  }
+
+  checkRankDistance(homeRank, compareRank) {
+    const ranks = "A23456789TJQK";
+    return ranks.indexOf(compareRank) - ranks.indexOf(homeRank);
+  }
+
+  isLastChild() {
+    return this.card.parentNode.lastChild === this.card;
+  }
+
   isStock() {
     return this.card.parentNode.classList.contains("game-board__stock");
+  }
+
+  isTableau() {
+    return this.card.parentNode.classList.contains("game-board__tableau");
+  }
+
+  isWaste() {
+    return this.card.parentNode.classList.contains("game-board__waste");
   }
 
   isFlipped() {
@@ -87,16 +149,14 @@ export default class Card {
     this.card.classList.toggle("card__flipped", faceIsUp);
   }
 
-  checkForFoundationOverlap() {
+  getOverlap(overlapTargets = []) {
     document.body.dispatchEvent(new Event("resetOverlapIndication"));
+
     const cardRect = this.card.getBoundingClientRect();
-    const foundationElements = document.querySelectorAll(
-      ".game-board__foundation"
-    );
     let maxOverlapArea = 0;
     let bestMatchElement = null;
 
-    foundationElements.forEach((element) => {
+    overlapTargets.forEach((element) => {
       const elementRect = element.getBoundingClientRect();
 
       const overlapX = Math.max(
@@ -117,8 +177,6 @@ export default class Card {
       }
     });
 
-    if (bestMatchElement) {
-      bestMatchElement.classList.add("game-board__positive-overlap");
-    }
+    return bestMatchElement;
   }
 }
