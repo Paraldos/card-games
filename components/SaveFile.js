2e8e3dec6ae1a6394e76c9fa41320a3409d875d4
Paraldos <hillbrecht.s@gmail.com>
2024-10-20 10:20:40 +0200

    prevent clickspamming on undo

--- components/SaveFile.js
+++ components/SaveFile.js
@@ -1,17 +1,21 @@
 export default class SaveFile {
   constructor() {
     this.saves = [];
+    this.undoIsRunning = false;
     document.body.addEventListener("resetSaveFile", () => (this.saves = []));
     document.body.addEventListener("pushSave", (e) => this.push(e.detail));
     document.body.addEventListener("undo", () => this.undo());
   }
 
   async undo() {
+    if (this.undoIsRunning) return;
+    this.undoIsRunning = true;
     for (let i = this.saves.length - 1; i >= 0; i--) {
       const stopLoop = this.undoLastAction(this.saves[i]);
       if (stopLoop) break;
       await new Promise((resolve) => setTimeout(resolve, 300));
     }
+    this.undoIsRunning = false;
   }
 
   undoLastAction(lastAction) {
