47e273fd32c6b16bbec18663524fcbe5b445a413
Paraldos <hillbrecht.s@gmail.com>
2024-10-20 16:19:05 +0200

    more footer styling

--- components/button.js
+++ components/button.js
@@ -2,16 +2,22 @@ import GameBoard from "./gameBoard.js";
 
 export default class Button {
   constructor() {
-    this.restartButton = document.querySelector(".restart-button");
+    this.restartButton = document.querySelector(".button__restart");
     this.undoButton = document.querySelector(".button__undo");
+    this.menuButton = document.querySelector(".button__menu");
+    this.menu = document.querySelector(".menu");
+    this.modalBackground = document.querySelector(".modal__background");
     this.GameBoard = new GameBoard();
     this.setupRestartButton();
     this.setupUndoButton();
+    this.setupMenuButton();
+    this.setupModalBackground();
   }
 
   setupRestartButton() {
     this.restartButton.addEventListener("click", () => {
       this.GameBoard.resetGameBoard();
+      this.disableMenu();
     });
   }
 
@@ -19,5 +25,21 @@ export default class Button {
     this.undoButton.addEventListener("click", () => {
       document.body.dispatchEvent(new CustomEvent("undo"));
     });
+  }
+
+  setupMenuButton() {
+    this.menuButton.addEventListener("touchend", () => {
+      this.menu.classList.toggle("menu--active");
+      this.modalBackground.classList.toggle("modal__background--active");
+    });
+  }
+
+  setupModalBackground() {
+    this.modalBackground.addEventListener("touchend", () => this.disableMenu());
+  }
+
+  disableMenu() {
+    this.menu.classList.remove("menu--active");
+    this.modalBackground.classList.remove("modal__background--active");
   }
 }
